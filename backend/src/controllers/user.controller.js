c39b868e8693916f310c6177ac827460c67898ef
ShettyVinith <vinithshetty9372@gmail.com>
2025-07-20 20:45:46 +0530

    Added Send Friend Request feature

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.js";
+import FriendRequest from "../models/FriendRequest.js";
 
 export async function getRecommendedUsers(req, res) {
   try {
@@ -33,5 +34,53 @@ export async function getMyFriends(req, res) {
   } catch (error) {
     console.error("Error fetching friends:", error.message);
     res.status(500).json({ message: "Error fetching friends" });
+  }
+}
+
+export async function sendFriendRequest(req, res) {
+  try {
+    const myId = req.user.id;
+    const { id: recipientId } = req.params;
+
+    //prevent sending friend request to self
+    if (myId === recipientId) {
+      return res
+        .status(400)
+        .json({ message: "You cannot send a friend request to yourself" });
+    }
+
+    // Check if recipient exists
+    const recipient = await User.findById(recipientId);
+    if (!recipient) {
+      return res.status(404).json({ message: "Recipient not found" });
+    }
+
+    // Check if a friend request already exists
+    if (recipient.friends.includes(myId)) {
+      return res.status(400).json({ message: "You are already friends" });
+    }
+
+    // Check if a pending friend request already exists
+    const existingRequest = await FriendRequest.findOne({
+      $or: [
+        { sender: myId, recipient: recipientId },
+        { sender: recipientId, recipient: myId },
+      ],
+    });
+
+    if (existingRequest) {
+      return res.status(400).json({ message: "Friend request already sent" });
+    }
+
+    // Create a new friend request
+    const friendRequest = await FriendRequest.create({
+      sender: myId,
+      recipient: recipientId,
+    });
+
+    res.status(201).json(friendRequest);
+  } catch (error) {
+    console.error("Error sending friend request:", error.message);
+    res.status(500).json({ message: "Error sending friend request" });
   }
 }
