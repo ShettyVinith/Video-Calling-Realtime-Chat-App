f4abfc24375c0727b3a7ab82de2e6d83a71df162
ShettyVinith <vinithshetty9372@gmail.com>
2025-07-20 20:53:04 +0530

    Added Accept Friend Request feature

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import {
+  acceptFriendRequest,
   getMyFriends,
   getRecommendedUsers,
   sendFriendRequest,
@@ -14,5 +15,6 @@ router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
 
 export default router;
